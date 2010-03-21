7a630a7a4067577ffe241fca0de54c27262a064e
andrei1089 <andrei.mirestean@gmail.com>
2010-03-21 14:39:03 +0200

    Update build script

--- build_pedo.py
+++ build_pedo.py
@@ -41,9 +41,10 @@ if __name__ == "__main__":
     #  p.preremove="""#!/bin/sh
     #  chmod +x /usr/bin/mclock.py""" #Set here your pre remove script
     version = "0.3"           #Version of your software, e.g. "1.2.0" or "0.8.2"
-    build = "2"                 #Build number, e.g. "1" for the first build of this version of your software. Increment for later re-builds of the same version of your software.
+    build = "3"                 #Build number, e.g. "1" for the first build of this version of your software. Increment for later re-builds of the same version of your software.
     #Text with changelog information to be displayed in the package "Details" tab of the Maemo Application Manager
-    changeloginformation ="New features: \n*fixed Imperial units bug\n*option to set sensitivity\n*options to set step length\n*calculate number of lost calories\n*show graphs of steps/distance/calories\n*save history in XML file\n*set alarm for steps/calories/distance\n*option to pause timer when not walking"
+    #changeloginformation ="New features: \n*fixed Imperial units bug\n*option to set sensitivity\n*options to set step length\n*calculate number of lost calories\n*show graphs of steps/distance/calories\n*save history in XML file\n*set alarm for steps/calories/distance\n*option to pause timer when not walking"
+    changeloginformation = open("debian/changelog").read()
     dir_name = "src"            #Name of the subfolder containing your package source files (e.g. usr\share\icons\hicolor\scalable\myappicon.svg, usr\lib\myapp\somelib.py). We suggest to leave it named src in all projects and will refer to that in the wiki article on maemo.org
     for root, dirs, files in os.walk(dir_name):
         real_dir = root[len(dir_name):]
